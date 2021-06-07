3d1429e2f60102a7e338939bc9f4b0defab1ca98
Kirill Ikonnikov <ikonnikov.kirill@gmail.com>
2021-06-07 17:04:59 +1200

    l4hw

---
+++ src/test/java/fourthapp/FourthappApplicationTests.java
@@ -0,0 +1,13 @@
+package fourthapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FourthappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
